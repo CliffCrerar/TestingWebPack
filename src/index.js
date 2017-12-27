957db8e47d0b4fb9c25afb5b4215404035bdaa5b
Cliff <cliff.crerar@gmail.com>
2017-12-28 00:04:05 +0200

    commit all

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
-
-
+import './style.css';
+import Icon from './city.jpg';
 
 function component() {
     var element = document.createElement('div');
@@ -9,6 +9,12 @@ function component() {
     // lodash is not imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    //add image to existing div
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
